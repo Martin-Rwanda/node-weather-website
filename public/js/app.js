864fd4fbd9ece28d08700be7596483b0e43eb497
Martin-Rwanda <ntezijmartin27@gmail.com>
2025-07-11 17:15:08 +0200

    changing the fetch in public/js/app.js so that the remote server can use it

--- public/js/app.js
+++ public/js/app.js
@@ -39,7 +39,7 @@ weatherForm.addEventListener('submit', (e) => {
     if (location.length === 0) {
         console.log('Please provide Location' )
     } else {
-        fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
+        fetch(`/weather?address=${location}`).then((response) => {
             response.json().then((data) => {
                 if (data.error) {
                     messageOne.textContent = data.error
